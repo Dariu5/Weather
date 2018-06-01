5f1f002b7b1fb021f44a02b11b6f360477d2e20c
Darius <radiocool@gmail.com>
2018-06-01 22:54:25 +0300

    First

--- Orai/Form1.Designer.cs
+++ Orai/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(520, 66);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nustatyti vietovę";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 73);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(492, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(865, 577);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Orai";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Orai/Form1.cs
+++ Orai/Form1.cs
@@ -2,19 +2,83 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Net;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Device.Location;
+using SimpleWifi;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Orai
 {
+
+
     public partial class Form1 : Form
     {
+         static string Google_API_Key;
+
         public Form1()
         {
             InitializeComponent();
+
+            StreamReader reader = new StreamReader("gkey.txt");
+
+            Google_API_Key = reader.ReadLine();
+        }
+       
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var response = await GetGeoData();
+
+            Geo coordinates = JsonConvert.DeserializeObject<Geo>(response);
+
+            var coordinates_string = coordinates.Location.Lat.ToString().Replace(',','.') + ", " + coordinates.Location.Lng.ToString().Replace(',', '.');
+
+
+            response = await GetCity(coordinates);
+
+
+            Adresas adresas = JsonConvert.DeserializeObject<Adresas>(response);
+
+            textBox1.Text = adresas.Results[1].FormattedAddress.ToString() + " (" + coordinates_string + ")";
+
+        }
+
+        private static async Task<string> GetGeoData()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string myJson = "";
+
+            var response = await httpClient.PostAsync("https://www.googleapis.com/geolocation/v1/geolocate?key=" + Google_API_Key,
+                new StringContent(myJson, Encoding.UTF8, "application/json"));
+            string content = await response.Content.ReadAsStringAsync();
+
+            return content;
+        }
+
+        private static async Task<string> GetCity(Geo data)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string myJson = 
+                
+                "latlng=" + data.Location.Lat.ToString().Replace(',', '.') + "," +data.Location.Lng.ToString().Replace(',', '.') + "&" + "key=" + Google_API_Key;
+
+            var send = new StringContent(myJson, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync("https://maps.googleapis.com/maps/api/geocode/json?" + myJson, send);
+                
+            string content = await response.Content.ReadAsStringAsync();
+
+            return content;
         }
     }
 }
